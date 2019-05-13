7fde12a6c42c5882091cade705fff4cf0b573043
Brody Currie <brodycurrie@gmail.com>
2019-05-13 17:48:23 -0400

    My emotions print out the way I want them to now

--- reinforcing_exercise.py
+++ reinforcing_exercise.py
@@ -6,6 +6,14 @@ emotions = {
     "joy": int(input('joy on a level of 1 - 3: ')),
 }
 
+# emotions = {
+#     "anger": 1,
+#     "disgust": 2,
+#     "fear": 3,
+#     "sadness": 2,
+#     "joy": 1
+# }
+
 class Person:
     """ This class represents a person object """
 
@@ -14,21 +22,31 @@ class Person:
         self.emotions = emotions
 
     def __str__(self):
-        for emotion, level in self.emotions.items():
-            return f"I am feeling a {self.find_emotions()} amount of {self.emotions}"
+        temp = []
+        ratings = {
+            '1': 'low',
+            '2': 'medium',
+            '3': 'high'
+        }
 
-    def find_emotions(self):
-        for level in self.emotions.values():
-            if level == 1:
-                return 'low'
-            elif level == 2:
-                return 'medium'
-            elif level == 3:
-                return 'high'
-            else:
-                return 'invalid number'
+        for emotion, amount in self.emotions.items():
+            temp.append(f"I am feeling a {ratings[str(amount)]} amount of {emotion}")
+
+        return '\n'.join(temp)
+
+    # def find_emotions(self):
+    #     for level in self.emotions.values():
+    #         if level == 1:
+    #             return 'low'
+    #         elif level == 2:
+    #             return 'medium'
+    #         elif level == 3:
+    #             return 'high'
+    #         else:
+    #             return 'invalid number'
 
 
 brody = Person('Brody', emotions)
 
 print(brody)
+
